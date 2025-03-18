8b94309727a0bf6768026afe2ef57276f2a6088b
mmakhalin <mmakhalin@phoenixit.ru>
2025-03-18 17:19:51 +0300

    Убрал лишнее из проекта

--- pharmacy-app/src/main/java/pharmacy/app/PharmacyAppStarter.java
+++ pharmacy-app/src/main/java/pharmacy/app/PharmacyAppStarter.java
@@ -2,8 +2,6 @@ package pharmacy.app;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pharmacy.app.service.MedicineGrpcServiceImpl;
@@ -11,7 +9,6 @@ import pharmacy.app.service.MedicineGrpcServiceImpl;
 import java.io.IOException;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Project Api", version = "1.0", description = "Сервис аптеки"))
 public class PharmacyAppStarter {
 
     /**
