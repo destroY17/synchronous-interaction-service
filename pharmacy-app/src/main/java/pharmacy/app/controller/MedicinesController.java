55e65394f6b68577a07d0462d64ce9328cb09ca8
mmakhalin <mmakhalin@phoenixit.ru>
2025-04-14 13:12:12 +0300

    Лекция 2

--- pharmacy-app/src/main/java/pharmacy/app/controller/MedicinesController.java
+++ pharmacy-app/src/main/java/pharmacy/app/controller/MedicinesController.java
@@ -1,5 +1,9 @@
 package pharmacy.app.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import pharmacy.app.api.MedicinesService;
 import pharmacy.app.dto.Medicine;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/medicines")
+@Tag(name = "Контроллер лекарств", description = "Контроллер для лекарств")
+@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Запрос выполнен успешно"),
+@ApiResponse(responseCode = "401", description = "Ошибка доступа(пользователь не авторизован)")})
 public class MedicinesController {
 
     private final MedicinesService medicinesService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получить лекарство из аптеки")
     public Medicine get(@PathVariable Long id) {
         return medicinesService.get(id);
     }
